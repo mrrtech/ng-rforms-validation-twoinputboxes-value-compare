b778ff0cbf2e703d8fe888f1e4f0af5060621cb4
Ramesh <ramesh.mavuluri.info@gmail.com>
2020-12-27 17:50:23 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-rforms-validations-twoinputboxes-value-compare';
+}
